e8f429c4f61f57487038a0f1f5658519d11256c8
Charles Anderson Florez Jaramillo <91999699+AndyGreyrat@users.noreply.github.com>
2022-08-19 14:37:08 -0500

    Presentacion Personal Anderson Florez Jaramillo

---
+++ Presentacion_Anderson/src/main/java/PresentacionAnderson.java
@@ -0,0 +1,35 @@
+public class PresentacionAnderson {
+    private String name;
+    private String presentation;
+    private String email;
+
+    public PresentacionAnderson(String name,String presentation, String email){
+        this.setName(name);
+        this.setPresentation(presentation);
+        this.setEmail(email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPresentation() {
+        return presentation;
+    }
+
+    public void setPresentation(String presentation) {
+        this.presentation = presentation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
