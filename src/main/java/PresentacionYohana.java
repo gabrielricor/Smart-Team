4a1c12edfd1d188c810562c1ec75e2c0e33d9cbf
Yohana <leyhurta12214@gmail.com>
2022-08-19 15:22:56 -0500

    Presentación Yohana

---
+++ src/main/java/PresentacionYohana.java
@@ -0,0 +1,8 @@
+public class PresentacionYohana {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Yohana Hurtado" +
+                "vivo en Barbosa Santander" +
+                " estudio  Tecnología en Electrónica Industrial" +
+                "me gusta la Literatura Fantástica y la programación.");
+    }
+}
