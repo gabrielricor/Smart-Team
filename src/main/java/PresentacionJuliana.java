9d831036e1abc8ef512cabd271cc45f833c2a9cd
Juliana Caicedo <110918758+Caicesoto@users.noreply.github.com>
2022-08-20 15:18:44 -0500

    Esta es mi presentación - Juliana Caicedo-

---
+++ src/main/java/PresentacionJuliana.java
@@ -0,0 +1,5 @@
+public class PresentacionJuliana {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Juliana Caicedo, soy abogada, me desempeño en el área laboral y actualmente soy asistente de gerencia en una fábrica de bocadillos");
+    }
+}
